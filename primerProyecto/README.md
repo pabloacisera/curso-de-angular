7dd01f6ec4311c75f2fccb64213ad3a603d6bc4c
TuNombreDeUsuario <pabloacisera2021@gmail.com>
2024-06-11 11:07:51 -0300

    creacion del primer proyecto

--- primerProyecto/README.md
+++ primerProyecto/README.md
@@ -1,4 +1,4 @@
-# ProyectoInicial
+# PrimerProyecto
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.3.
 
--- primerProyecto/src/app/app.component.spec.ts
+++ primerProyecto/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'proyecto-inicial' title`, () => {
+  it(`should have the 'primerProyecto' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyecto-inicial');
+    expect(app.title).toEqual('primerProyecto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyecto-inicial');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, primerProyecto');
   });
 });
