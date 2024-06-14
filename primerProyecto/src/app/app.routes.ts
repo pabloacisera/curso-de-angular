9cfe457014557a92bca0daa792adf48f33e327b0
TuNombreDeUsuario <pabloacisera2021@gmail.com>
2024-06-13 21:41:03 -0300

    preparacion del proyecto

--- primerProyecto/src/app/app.routes.ts
+++ primerProyecto/src/app/app.routes.ts
@@ -1,16 +1,4 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: "dashboard",
-    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
-    children: [
-      // Añadir rutas secundarias aquí si es necesario
-    ]
-  }, 
-  {
-    path: "",
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  }
+export const routes: Routes = [ 
 ];
